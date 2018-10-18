3f0181f6bc4cec89419d9d24dadb27cc2a767271
TasyaAmaliaa <tasya_salsabila_25rpl@student.smktelkom-mlg.sch.id>
2018-10-18 19:14:16 +0700

    + membuat class FoodDrink

---
+++ app/src/main/java/com/example/tasya/tablayoutrecyclerview2/FoodDrink.java
@@ -0,0 +1,20 @@
+package com.example.tasya.tablayoutrecyclerview2;
+
+public class FoodDrink {
+    private String title;
+
+    public FoodDrink() {
+    }
+
+    public FoodDrink(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+}
