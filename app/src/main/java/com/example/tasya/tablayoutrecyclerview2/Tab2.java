fe3b7098f6e904efda80af1c49fd9e8a7f0a416d
TasyaAmaliaa <tasya_salsabila_25rpl@student.smktelkom-mlg.sch.id>
2018-11-09 17:47:01 +0700

    Menambahkan fragment API dan google_maps_api di values

--- app/src/main/java/com/example/tasya/tablayoutrecyclerview2/Tab2.java
+++ app/src/main/java/com/example/tasya/tablayoutrecyclerview2/Tab2.java
@@ -2,6 +2,7 @@ package com.example.tasya.tablayoutrecyclerview2;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -32,6 +33,7 @@ public class Tab2 extends Fragment {
     Realm realm;
     RealmHelper realmHelper;
     List<FavoriteModel> favoriteModel;
+    SwipeRefreshLayout sw;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,17 +51,45 @@ public class Tab2 extends Fragment {
         recyclerView.setLayoutManager(layoutManager);
         //mAdapter = new FoodAdapter(mList, getContext());
         //recyclerView.setAdapter(mAdapter);
-
+        sw = view2.findViewById(R.id.swLayout);
         //Set up Realm
         RealmConfiguration configuration = new RealmConfiguration.Builder().build();
         realm = Realm.getInstance(configuration);
         realmHelper = new RealmHelper(realm);
 
+        //tampil semua data favorite
         favoriteModel = realmHelper.getFavorite();
         vAdapter = new AdapterFavorite(getActivity(), favoriteModel);
         recyclerView.setAdapter(vAdapter);
+
+        //Mengeset listener yang akan dijalankan saat di refresh
+        sw.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //handler untuk menjalankan jeda selama 2 detik
+//                new Handler().postDelayed(new Runnable(){
+//                    @Override
+//                    public void run(){
+//                        sw.setRefreshing(false);//berhenti ketika data sudah di load
+//
+//                        //fungsi yang dijalankan saat di refresh
+//                        favoriteModel = realmHelper.getFavorite();
+//                        vAdapter = new AdapterFavorite(getActivity(), favoriteModel);
+//                        recyclerView.setAdapter(vAdapter);
+//                    }
+//                }, 2000);
+            }
+        });
         return view2;
     }
 
-
+    //lifecycle ,biar bisa langsung refresh saat ditekan tab nya
+    @Override
+    public void onResume() {
+        super.onResume();
+        sw.setRefreshing(false);
+        favoriteModel = realmHelper.getFavorite();
+        vAdapter = new AdapterFavorite(getActivity(), favoriteModel);
+        recyclerView.setAdapter(vAdapter);
+    }
 }
